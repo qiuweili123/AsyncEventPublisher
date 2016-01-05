c15f74377f8de465c7f05cff95e7fed321483da9
huipeng <huipeng@5151study.com>
2016-01-05 12:54:17 +0800

    fix issue

--- src/main/java/huipeng/daoapp/io/async/MessageNotification.java
+++ src/main/java/huipeng/daoapp/io/async/MessageNotification.java
@@ -2,8 +2,6 @@ package huipeng.daoapp.io.async;
 
 import java.io.IOException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -12,7 +10,6 @@ import huipeng.daoapp.io.service.MessageService;
 
 @Component
 public class MessageNotification {
-  private static final Logger logger = LoggerFactory.getLogger(MessageNotification.class);
 
   @Autowired
   private MessageService messageService;
